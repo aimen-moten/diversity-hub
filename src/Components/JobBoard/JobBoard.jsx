163942f173637e6f7006a0c3ecc1dcea5d329929
Aimen Moten <aimen.moten@gmail.com>
2023-04-27 16:28:39 -0400

    job board

---
+++ src/Components/JobBoard/JobBoard.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import JobsLanding from "./JobsLanding";
+import Jobs from "./Jobs";
+
+export default function JobBoard(){
+    return (
+        <div>
+            <JobsLanding/>
+            <Jobs />
+        </div>
+    )
+}
